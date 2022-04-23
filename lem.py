4215fdd77feea1fad77376cd521f4293433d4df6
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-23 07:27:08 +0200

    format fix

--- lem.py
+++ lem.py
@@ -5,8 +5,9 @@ ALL_POSSIBLE_ATTRIBUTES = getAllPossibleAttributes()
 INPUT_DATA_LENGTH = len(DATA)
 GENERATED_RULES = []
 PARSED_PAIRS = getParsedPairs()
-print("PARSED_PAIRS: " + str(PARSED_PAIRS))
+
+parsedRecordsAndRules = []
 
 for x in range(INPUT_DATA_LENGTH):
     for pair in PARSED_PAIRS:
-        print(DATA[x])
+        print()

5c1b018e4a23dc1733f65ff805d8dc778f5dd86e
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-23 19:34:02 +0200

    classes

--- lem.py
+++ lem.py
@@ -2,6 +2,7 @@ from parser import parseInputData, getAllPossibleAttributes, getKeyAttribute, ge
 DATA = parseInputData()
 KEY_ATTRIBUTE = getKeyAttribute()
 ALL_POSSIBLE_ATTRIBUTES = getAllPossibleAttributes()
+ALL_CLASSES = ALL_POSSIBLE_ATTRIBUTES[KEY_ATTRIBUTE]
 INPUT_DATA_LENGTH = len(DATA)
 GENERATED_RULES = []
 PARSED_PAIRS = getParsedPairs()
@@ -11,11 +12,22 @@ parsedRecordsAndPairs = {}
 for x in range(INPUT_DATA_LENGTH):
     for pair in PARSED_PAIRS:
         if DATA[x][list(pair)[0]] == pair[list(pair)[0]]:
-            parsedName = str(list(pair)[0]) + str(pair[list(pair)[0]])
+            parsedName = str(list(pair)[0]) + '_' + str(pair[list(pair)[0]])
             if parsedRecordsAndPairs.get(parsedName) is None:
                 parsedRecordsAndPairs[parsedName] = [DATA[x]]
             else:
                 parsedRecordsAndPairs[parsedName].append(DATA[x])
 
-for attr, value in parsedRecordsAndPairs.items():
-    print(attr, value)
+B = []
+for x in range(INPUT_DATA_LENGTH):
+    if DATA[x][KEY_ATTRIBUTE] == ALL_CLASSES['srednie']:
+        B.append(DATA[x])
+
+for item in B:
+    print(item)
+
+G = B
+T = []
+
+# for attr, value in parsedRecordsAndPairs.items():
+#     print(attr, value)

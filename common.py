b306b5290dd98bf5b26f23ebca8854d5af15f5d5
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-24 15:50:26 +0200

    fixes

--- common.py
+++ common.py
@@ -37,13 +37,3 @@ def getPL(PL):
         if item['pl'] == mostRelatedPair:
             mostRelatedPair = item
     return mostRelatedPair
-    # mostRelatedPairs = []
-    # P = max(P)
-    # for item in PL:
-    #     if item['pl'][0] == P:
-    #         L.append(item['pl'][1])
-    # L = min(L)
-    # for item in PL:
-    #     if item['pl'][0] == P and item['pl'][1] == L:
-    #         mostRelatedPairs.append(item)
-    # return mostRelatedPairs

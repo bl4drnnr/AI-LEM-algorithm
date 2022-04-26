405e55478acbf28162bfb9ac81d0b7a4147665be
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-26 19:13:27 +0200

    getPL new func

--- common.py
+++ common.py
@@ -12,35 +12,74 @@ def lookForRuleAndRecordMatches(record, rule):
     return [len(t), len(record)]
 
 
-def getPL(PL):
-    mostRelatedPair = None
-    pl = []
-    P = []
-    L = []
-    for item in PL:
-        pl.append(item['pl'])
+def getPL(records):
+    Ps = []
+    Ls = []
 
-    for item in pl:
-        P.append(item[0])
+    # Look for max P and min L
+    for item in records:
+        Ps.append(item['PL'][0])
+        Ls.append(item['PL'][1])
 
-    P = max(P)
-    temp = []
-    for item in pl:
-        if item[0] == P:
-            temp.append(item)
+    PsAndLs = []
 
-    for item in temp:
-        L.append(item[1])
+    for x in range(len(Ps)):
+        PsAndLs.append([Ps[x], Ls[x]])
 
-    L = min(L)
+    maxP = max(Ps)
+    tempPsAndLs = []
+    tempLs = []
 
-    for item in pl:
-        if item[0] == P and item[1] == L:
-            mostRelatedPair = item
-    for item in PL:
-        if item['pl'] == mostRelatedPair:
-            mostRelatedPair = item
-    return mostRelatedPair
+    for item in PsAndLs:
+        if item[0] == maxP:
+            tempPsAndLs.append(item)
+
+    for item in tempPsAndLs:
+        tempLs.append(item[1])
+
+    minL = min(tempLs)
+    maxPandMinL = None
+
+    for item in PsAndLs:
+        if item[0] == maxP and item[1] == minL:
+            maxPandMinL = item
+
+    for record in records:
+        if record['PL'] == maxPandMinL:
+            maxPandMinL = record
+
+    return maxPandMinL
+
+
+# def getPL(PL):
+#     mostRelatedPair = None
+#     pl = []
+#     P = []
+#     L = []
+#     for item in PL:
+#         pl.append(item['pl'])
+#
+#     for item in pl:
+#         P.append(item[0])
+#
+#     P = max(P)
+#     temp = []
+#     for item in pl:
+#         if item[0] == P:
+#             temp.append(item)
+#
+#     for item in temp:
+#         L.append(item[1])
+#
+#     L = min(L)
+#
+#     for item in pl:
+#         if item[0] == P and item[1] == L:
+#             mostRelatedPair = item
+#     for item in PL:
+#         if item['pl'] == mostRelatedPair:
+#             mostRelatedPair = item
+#     return mostRelatedPair
 
 
 def extractIndexes(arr):
